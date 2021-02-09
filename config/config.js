3654203e622ef406e005a97d7bf62094d1536164
DemarioDouce <demario.douce567@gmail.com>
2021-02-08 23:21:07 -0500

    Changes and improvements.

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
-﻿module.exports = require("./env/" + process.env.NODE_ENV + ".js");
+﻿//This file simply loads the correct configuration file.
+//According to the process.env.NODE_ENV environment variable.
+//Which is set in server.js (it's value is 'development').
+//This code will in fact return ./env/development.js.
+module.exports = require("./env/" + process.env.NODE_ENV + ".js");
--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,8 @@
-﻿module.exports = {
+﻿//Development configuration options.
+//To sign the session identifier, use a secret string.
+module.exports = {
+  //Local server for mongodb.
+  db: "mongodb://localhost/lab2-ex1",
+
   sessionSecret: "developmentSessionSecret",
 };
